3d75a4fd495394717808227e9a4115dfd0b4d697
Soumyadeep Datta <soumyadeepdatta34.alternative@gmail.com>
2023-02-20 01:30:59 +0530

    edu done

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,17 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function Education(props) {
   return (
-    <div>
+    <Container style={{marginTop:"50px",marginBottom:"50px"}}>
+      <h1>Education</h1>
       <ul>
         {props.data.map((e, id) => (
           <li key={id}>
-            <h1>{e.degree}</h1>
-            <p>{JSON.stringify(e)}</p>
+            <h2>{e.degree} ({e.start} - {e.end})</h2>
+            <p>{e.organization} <br /> {e.board} : {e.score} {e.score_unit} </p>
           </li>
         ))}
       </ul>
-    </div>
+    </Container>
   );
 }
 
