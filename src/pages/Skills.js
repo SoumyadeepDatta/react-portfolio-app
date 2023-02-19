4358a452343847ee2bdc0855cf10d43a5d93dd28
Soumyadeep Datta <soumyadeepdatta34.alternative@gmail.com>
2023-02-20 01:20:33 +0530

    skills done

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,28 +1,44 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Skills(props) {
   return (
-    <div>
-      <ul>
-        {props.data[0].map((e, id) => (
-          <li key={id}>{e}</li>
-        ))}
-      </ul>
-      <ul>
-        {props.data[1].map((e, id) => (
-          <li key={id}><a href={e.url}>{e.name}</a></li>
-        ))}
-      </ul>
+    <Container >
+      <Row style={{ marginTop: "50px", marginBottom: "50px" }}>
+        <Col md={6}>
+          <h1>Skills</h1>
+          <ul>
+            {props.data[0].map((e, id) => (
+              <li key={id}>{e}</li>
+            ))}
+          </ul>
+        </Col>
+        <Col md={6}>
+          <h1>Profiles</h1>
+          <ul>
+            {props.data[1].map((e, id) => (
+              <li key={id}>
+                <a href={e.url}>{e.name}</a>
+              </li>
+            ))}
+          </ul>
+        </Col>
+      </Row>
       <hr />
-      <ul>
-        {props.data[2].map((e, id) => (
-          <li key={id}>
-            <Link to={`/certifications/${e.id}`}>{e.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <Row style={{ marginTop: "50px", marginBottom: "50px" }}>
+        <Col md={6}>
+          <h1>Certifications</h1>
+          <ul>
+            {props.data[2].map((e, id) => (
+              <li key={id}>
+                <Link to={`/certifications/${e.id}`}>{e.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
