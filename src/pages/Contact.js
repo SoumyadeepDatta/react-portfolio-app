6cc6e319d41b5728bda17b19e9da2151b556f75a
Soumyadeep Datta <soumyadeepdatta34.alternative@gmail.com>
2023-02-20 22:44:17 +0530

    contact done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function Contact(props) {
   return (
-    <div>
-      {JSON.stringify(props.data[0])}
-      <hr />
+    <Container style={{ marginTop: "50px", paddingBottom: "50px" }}>
+      <h1>Personal Details</h1>
+      <ul>
+        <li>
+          <u>Address:</u> {props.data[0].address}
+        </li>
+        <li>
+          <u>Languages Known:</u> {props.data[0].languages.join(", ")}
+        </li>
+        <li>
+          <u>Date of Birth:</u> {(new Date(props.data[0].dob).toString())}
+        </li>
+        <li>
+          <u>Nationality:</u> {props.data[0].nationality}
+        </li>
+        <li>
+          <u>Phone:</u> {props.data[0].phone}
+        </li>
+        <li><a href="">Download Resume</a></li>
+      </ul>
+
+      <hr style={{marginTop:"50px",marginBottom:"50px"}}/>
+      <h1>Connect</h1>
       <ul>
         {props.data[1].map((e, id) => (
           <li key={id}>
@@ -14,7 +35,7 @@ function Contact(props) {
           </li>
         ))}
       </ul>
-    </div>
+    </Container>
   );
 }
 
